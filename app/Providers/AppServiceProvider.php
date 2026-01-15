6f442a55eefbfa3b7e010b926e284544a3317550
feosorio <fe.osorio@ccgrupo.com.co>
2026-01-15 11:25:38 -0500

    observers - providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Observers\ContactObserver;
-use App\Observers\ManagementAliadoObserver;
-use App\Observers\ManagementAfiliadoObserver;
-use App\Observers\SpecialCaseObserver;
+
+// CONTACTOS - OBSERVERS
+use App\Observers\Affiliate\AffiliateContactObserver;
+use App\Observers\Alliance\AllianceContactObserver;
+
+// GESTIONES - OBSERVERS
+use App\Observers\Affiliate\AffiliateManagementObserver;
+use App\Observers\Alliance\AllianceManagementObserver;
+
+// CASOS ESPECIALES - OBSERVERS
+use App\Observers\Affiliate\AffiliateSpecialCaseObserver;
+use App\Observers\Alliance\AllianceSpecialCaseObserver;
+
+// CONTACTOS - MODELS
 use App\Models\Contact;
+
+// GESTIONES - MODELS
 use App\Models\Aliados\Management as ManagementAliados;
 use App\Models\Afiliados\Management as ManagementAfiliados;
+
+// CASOS ESPECIALES - MODELS
 use App\Models\SpecialCases;
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,9 +40,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Contact::observe(ContactObserver::class);
-        ManagementAliados::observe(ManagementAliadoObserver::class);
-        ManagementAfiliados::observe(ManagementAfiliadoObserver::class);
-        SpecialCases::observe(SpecialCaseObserver::class);
+        // contactos
+        AllianceContact::observe(AllianceContactObserver::class);
+        AffiliateContact::observe(AffiliateContactObserver::class);
+
+        // gestiones
+        AllianceManagement::observe(AllianceManagementObserver::class);
+        AffiliateManagement::observe(AffiliateManagementObserver::class);
+
+        // casos especiales
+        AllianceSpecialCases::observe(AllianceSpecialCaseObserver::class);
+        AffiliateSpecialCases::observe(AffiliateSpecialCaseObserver::class);
     }
 }
